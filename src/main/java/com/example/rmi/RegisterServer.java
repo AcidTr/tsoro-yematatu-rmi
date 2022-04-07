74ed1686fa3d384b86041aed5c02ff7371ba4df9
Victor Mourão <victor_murati@hotmail.com>
2022-04-06 23:16:17 -0300

    Add maven to build jar files

--- src/main/java/com/example/rmi/RegisterServer.java
+++ src/main/java/com/example/rmi/RegisterServer.java
@@ -1,30 +1,48 @@
 package com.example.rmi;
 
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
 public class RegisterServer {
 
+    RegisterServer(String hostname, int port) {
+        try {
+
+        LocateRegistry.createRegistry(port);
+
+        ServerRMI obj = new ServerRMI();
+
+        Naming.rebind("rmi://" + hostname + ":" + port + "/Server", obj);
+
+        System.out.println("Servidor Registrado!");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]) {
         try {
-//            if (args.length < 2) {
-//                throw new Error("Please provide the hostname and port. e.g. java Registrador hostname 1099");
-//            }
-
             String hostname = "localhost";
+
+
+            if(args.length > 0 && !args[0].isEmpty()){
+                hostname = args[0];
+                System.out.println(hostname);
+            }
 
             int port = 1099;
 
-//            System.setProperty("java.rmi.server.hostname", hostname);
+            if(args.length > 1 && !args[1].isEmpty()){
+                port = Integer.parseInt(args[1]);
+                System.out.println(port);
+            }
 
-            LocateRegistry.createRegistry(port);
-
-            ServerRMI obj = new ServerRMI();
-
-            Naming.rebind("rmi://" + hostname + ":" + port + "/Server", obj);
-
-            System.out.println("Servidor Registrado!");
-
+            new RegisterServer(hostname, port);
+            
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Erro");
